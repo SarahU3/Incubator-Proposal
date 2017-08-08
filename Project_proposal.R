a682fdc722a66e6adb80f9915b3cadc71c7f9eb4
Sarah U <sju2@georgetown.edu>
2017-08-08 00:06:21 -0400

    added files via upload
    changed repository name - need to combine files and re-clone to desktop

--- Project_proposal.R
+++ Project_proposal.R
@@ -1,32 +1,45 @@
 setwd("~/GitHub/DataIncubator")
 
 library(ggplot2)
-solar <- read.csv("~/GitHub/DataIncubator/openpv_all.csv")
-#some basic data cleaning to collapse levels of factor solar$install_type
-unique(solar$install_type)
-levels(solar$install_type)[levels(solar$install_type)=="Residential"] <- "residential"
-levels(solar$install_type)[levels(solar$install_type)==""] <- "unknown"
-levels(solar$install_type)[levels(solar$install_type)=="Commerical"] <- "Commercial"
-levels(solar$install_type)[levels(solar$install_type)=="Unknown"] <- "unknown"
-levels(solar$install_type)[levels(solar$install_type)=="Commercial - Small Business"] <- "Commercial"
-levels(solar$install_type)[levels(solar$install_type)=="Commercial - Agriculture"] <- "Commercial"
-levels(solar$install_type)[levels(solar$install_type)=="Commercial - Builders"] <- "Commercial"
-levels(solar$install_type)[levels(solar$install_type)=="Commercial - Other"] <- "Commercial"
-levels(solar$install_type)[levels(solar$install_type)=="Small Business"] <- "Commercial"
-levels(solar$install_type)[levels(solar$install_type)=="Agricultural"] <- "Commercial"
-levels(solar$install_type)[levels(solar$install_type)=="agriculture"] <- "commercial"
-levels(solar$install_type)[levels(solar$install_type)=="Commercial"] <- "commercial"
-table(solar$install_type)
+
 
 #plot 1 - cost per watt of the top 5 solar states
-sum(is.na(solar$state))
-table(solar$state) #top 5: AZ, CA, MA, NJ, NY
-topsolar <- subset(solar, state=="AZ" | state=="CA" | state=="MA" | state=="NJ" | state=="NY")
+sum(is.na(solarpv$state))
+table(solarpv$state) #top 5: AZ, CA, MA, NJ, NY
+topsolar <- subset(solarpv, state=="AZ" | state=="CA" | state=="MA" | state=="NJ" | state=="NY")
 
 ggplot(topsolar, aes(x=topsolar$state, y=topsolar$cost_per_watt, fill=topsolar$state)) + geom_boxplot() + ylab("Cost per watt ($)") + xlab("State") +
   guides(fill=FALSE) + ggtitle("Top Five States - Solar Installation Costs") + theme_classic()
 
 #plot 2 - size versus rebate
-solar$numrebate <- as.numeric(solar$rebate)
-residential <- subset(solar, install_type=="residential" & solar$annual_PV_prod<300000000)
+solarpv$numrebate <- as.numeric(solarpv$rebate)
+residential <- subset(solarpv, install_type=="residential" & solarpv$annual_PV_prod<300000000)
 ggplot(residential, aes(x=residential$size_kw, y=residential$numrebate)) + geom_point() + geom_smooth(colour="#2ca25f") + labs(x = "Size (KW)", y = "Rebate or Grant ($)", title="Size of installation and rebates", subtitle = "For residential installations") + theme_classic()
+
+# mapping
+library(maps)
+library(maptools)
+library(dplyr)
+solarpv12_15 <- solarpv[which(solarpv$installyear>=2012 & solarpv$installyear<=2015),]
+zipcodepv <- count(solarpv12_15, zipcode)
+zipcodepv$zipfive <- sprintf("%05d", zipcodepv$zipcode)
+statepv <- count(solarpv12_15, state)
+names(statepv) <- c("state2", "value")
+statepv$state <- state.name[match(statepv$state2,state.abb)]
+statepv$state <- tolower(statepv$state)
+statepv$state[statepv$state2=="DC"] <- "district of columbia"
+###zipmap <- readShapeSpatial("~/GitHub/DataIncubator/2015_us_zcta/cb_2015_us_zcta510_500k.shp")
+###statemap <- readShapeSpatial("~/GitHub/DataIncubator/2015_us_state/cb_2015_us_state_500k.shp")
+###zipmapdf <- fortify(zipmap)
+###statemapdf <- fortify(statemap)
+statemap <- map_data("state")
+ggplot(statepv, aes(map_id=state)) + geom_map(aes(fill = value), map = statemap) + 
+  expand_limits(x = statemap$long, y = statemap$lat)
+statepv$pct <- statepv$value/sum(statepv$value)*100
+ggplot(statepv, aes(map_id=state)) + geom_map(aes(fill = pct), map = statemap) + 
+  expand_limits(x = statemap$long, y = statemap$lat) +theme_bw() + 
+  labs(title="Percent of solar installations by state", fill="Percent") + 
+  scale_fill_gradient(low="gray90", high="green4")
+
+##CAzipcodepv <- count(solarpv12_15, zipcode)
+##zipcodepv$zipfive <- sprintf("%05d", zipcodepv$zipcode)
---
+++ SourceFile.R
@@ -0,0 +1,69 @@
+setwd("~/GitHub/DataIncubator")
+
+#load openpv data
+solar <- read.csv("~/GitHub/DataIncubator/openpv_all.csv", stringsAsFactors = FALSE)
+#some basic data cleaning to collapse levels of factor solar$install_type
+unique(solar$install_type)
+solar$install_type2 <- `levels<-`(factor(solar$install_type), list(agriculture=c("Agricultural","agricultural",
+                                                          "agriculture","Commercial - Agriculture"), 
+                                            commercial=c("Commercial","commercial","Commercial - Other",
+                                                         "Commerical","Commercial - Agriculture",
+                                                         "Small Business","Commercial - Small Business"),
+                                            educational=c("Educational","educational","education"),
+                                            institutional=c("Nonprofit","government","Government","nonprofit",
+                                                            "Municipal","public","Institutional","Public",
+                                                            "Gov't/NP"),
+                                            residential=c("residential","Residential","Residential/SF"),
+                                            utility=c("Utility","utility"),
+                                            unknown=c("Unknown","","unknown","Customer","Nonresidential","Not Stated")))
+unique(solar$install_type2)
+
+# load utility rates by zip
+setwd("~/GitHub/DataIncubator/Rates")
+files <- list.files(path = "~/GitHub/DataIncubator/Rates", pattern="*.csv")
+# First apply read.csv, then rbind
+rates <- data.frame()
+rates <- do.call(rbind, lapply(files, function(x) cbind(read.csv(x), name=strsplit(x,'\\.')[[1]][1])))
+save(rates, file = "~/GitHub/DataIncubator/rates.csv")
+rm(files)
+setwd("~/GitHub/DataIncubator")
+load("~/GitHub/DataIncubator/rates.csv")
+
+library(tidyr)
+rates <- rates %>% separate(name, c("iou | noniou","201*"), "zipcodes")
+names(rates)[names(rates) == '201*'] <- 'year'
+# how to combine
+
+solar$install <- as.Date(solar$date_installed, format = "%m/%d/%Y")
+solar$installyear <- as.numeric(format(solar$install, "%Y"))
+
+solarpv <- solar[c(-(31:81))]
+
+
+# trying to add other data
+
+library(plyr)
+library(xlsx)
+library(xlsxjars)
+library(rJava)
+#download all available data: tax returns by zip code
+years <- c(1999, 2001, 2002,(seq(2004,2010)))
+temp <- tempfile()
+download.file("https://www.irs.gov/pub/irs-soi/1998zipcode.zip",temp, mode="wb")
+unzipped <- unzip(temp, exdir=getwd())
+filenames <- list.files(path = "C:/Users/Sarah/Documents/GitHub/DataIncubator/1998ZIPCode", pattern = "xls")
+soi <- read.xlsx("~/GitHub/DataIncubator/1998ZIPCode/98zp01al.xlsx", 1, colIndex = c(1,2,5), startRow = 8, endRow = 4825)
+
+
+#import DSIRE data on incentives
+library(XML)
+library(rvest)
+library(dplyr)
+urldsire <- "https://ncsolarcen-prod.s3.amazonaws.com/fullexports/dsire-2017-08.xml"
+download.file(urldsire, "~/GitHub/DataIncubator/dsire-2017-08.xml")
+xmlfile <- xmlParse("~/GitHub/DataIncubator/dsire-2017-08.xml")
+class(xmlfile) 
+xmltop = xmlRoot(xmlfile)
+class(xmltop)
+xmlName(xmltop)
+xmlSize(xmltop)
